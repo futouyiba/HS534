65600247e38f0cac7a72aef1494f22c4e096ecc3
futouyiba <futouyiba@gmail.com>
2016-09-04 17:50:35 +0800

    net Control Flow Reconstruction

---
+++ Assets/HttpManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using BestHTTP;
+using UnityEngine.UI;
+using HutongGames.PlayMaker;
+
+
+public class HttpManager : MonoBehaviour {
+	public string ServerAddress;
+	public Text text;
+	public PlayMakerFSM GameMFSM;
+	public string URLSuffix;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void GetFromServer(){
+		HTTPRequest request = new HTTPRequest (new System.Uri (ServerAddress+"/"+URLSuffix), OnGetFinished);
+		request.Send();
+	}
+
+	public void SendNewGame(){
+		HTTPRequest request = new HTTPRequest (new System.Uri (ServerAddress + "/newgame"), OnGetFinished);
+		request.Send();
+	}
+
+	public void OnGetFinished(HTTPRequest request, HTTPResponse response){
+		string respText ;
+		respText = response.DataAsText;
+		Debug.Log("Request Finished! Text received: " + response.DataAsText);
+//		text.text = "Request Finished! Text received: " + response.DataAsText;
+		if (respText == "1"){
+			GameMFSM.SendEvent ("NetSuccess");
+
+		}
+	}
+
+	void parseJson(string jsonData){
+		
+	}
+
+}
